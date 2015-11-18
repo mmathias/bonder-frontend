d16205e8bd67d2c1a55ac4af4058285e5b28d751
Magno Mathias <mmathias@afilias.info>
2015-11-18 14:12:55 +0000

    Fixing results

--- app/results/results.js
+++ app/results/results.js
@@ -14,7 +14,7 @@ angular.module('myApp.results', ['ngRoute'])
   $http.get(API_URL + '/users/' + localStorage.challenged_user_id + '/answers')
   .then(function(response) {
     if (response.data.Answers && response.data.Answers.length) {
-      $score.total = response.data.Answers.length;
+      $scope.total = response.data.Answers.length;
       return $http.get(API_URL + '/users/' + localStorage.user_id + '/score/' + localStorage.challenged_user_id)
       .then(function(response) {
         $scope.score = response.data.Score[0].score;
