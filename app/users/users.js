63fce8a61ce737672bed4c13c07dba0ded74ff8e
Magno Mathias <mmathias@afilias.info>
2015-11-24 22:46:44 +0000

    Fixing promises

--- app/users/users.js
+++ app/users/users.js
@@ -18,7 +18,7 @@ angular.module('myApp.users', ['ngRoute', 'ui.gravatar'])
     for (var i = 0; i < $scope.users.length; i++) {
       var user = $scope.users[i];
 
-      if (user.user_email === localStorage.user_email) {
+      if (user.user_id == localStorage.user_id) {
           $scope.users.splice(i, 1);
           break;
       }
@@ -35,16 +35,22 @@ angular.module('myApp.users', ['ngRoute', 'ui.gravatar'])
       .then(function(responses) {
         var reqs = [], response;
         for (var i = 0; i < responses.length; i++) {
-          response = responses[i]
+          response = responses[i];
           if (response.data.Answers && response.data.Answers.length) {
-            $scope.users[i].total = response.data.Answers.length;
-            reqs.push($http.get(CONFIG.API_URL + 'users/' + localStorage.user_id + '/score/' + $scope.users[i].user_id));
+            var userId = findUserIndex($scope.users, response.data.Answers[0].user_id, 'user_id');
+            $scope.users[userId].total = response.data.Answers.length;
+            reqs.push($http.get(CONFIG.API_URL + 'users/' + localStorage.user_id + '/score/' + $scope.users[userId].user_id));
           }
         }
 
         $q.all(reqs).then(function(responses) {
           for(var i = 0; i<responses.length; i++) {
-            $scope.users[i].score = responses[i].data.Score[0].score;
+            var response = responses[i];
+            var userId = findUserIndex($scope.users, +response.data.Score[0].challenged_user_id, 'user_id');
+            $scope.users[userId].score = response.data.Score[0].score;
+            console.log($scope.users[userId].name);
+            console.log($scope.users[userId].score);
+            console.log($scope.users[userId].total);
           }
         });
       });
@@ -53,6 +59,15 @@ angular.module('myApp.users', ['ngRoute', 'ui.gravatar'])
   $scope.openChallenge = function(user) {
     localStorage.challenged_user_id = user.user_id;
     $location.url("/challenge");
+  }
+
+  var findUserIndex = function(array, value, property) {
+    var i;
+    for(i = 0; i < array.length; i++) {
+      if (array[i][property] === value) {
+        return i;
+      }
+    }
   }
 
 }]);
